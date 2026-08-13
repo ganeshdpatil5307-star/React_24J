951db3ba37a7774ee0f4b5d035b684c0b8603aad
Ganesh Patil <ganeshdpatil5307@gmail.com>
2026-08-13 16:39:32 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,7 +30,7 @@ export default function Login() {
       <form className="login-card" onSubmit={handleSubmit}>
 <<<<<<< HEAD
 <<<<<<< HEAD
-        <h1 className="login-title">Sign Inn</h1>
+        <h1 className="login-title">Sign Innn</h1>
 =======
         <h1 className="login-title">Sign In With New</h1>
 >>>>>>> d4c6ee980270ebc6047993ab2f1872c43fee6f98
